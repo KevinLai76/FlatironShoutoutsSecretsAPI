8e465f770adb52c3abbc85a1ea88d84fd9781c6b
Kevin Lai <kevinlai76@gmail.com>
2019-08-29 21:11:25 -0400

    auth login & signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+    def encode_token(payload)
+        JWT.encode(payload, "password", "HS256")
+    end
+
+    def user_payload(user)
+        { user_id: user.id }
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,8 @@ Rails.application.routes.draw do
   resources :shoutout_likes
   resources :secret_likes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  post '/login', to: 'auth#login' 
+  # post '/signup' to: 'users#create'
+  # get 'users/:id' to: 'users#show'
 end
