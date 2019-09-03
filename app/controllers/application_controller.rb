632e32fca60f5bf713bea861577fa4f0c34aa8cd
Kevin Lai <kevinlai76@gmail.com>
2019-09-03 16:55:58 -0400

    auth set up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,22 @@ class ApplicationController < ActionController::API
     def encode_token(payload)
         JWT.encode(payload, "password", "HS256")
     end
-
+    
     def user_payload(user)
         { user_id: user.id }
     end
+    
+    def token 
+        request.headers["Authorization"].split(" ")[1]
+    end
+
+    def decode_token
+        JWT.decode(token, "password", true, { algorithm: "HS256" })
+    end
+
+    def current_user
+        user_id = decode_token[0]["user_id"]
+        User.find(user_id)
+    end
+
 end
