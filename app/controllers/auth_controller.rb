67dbba9fb9e099d4ce5d69f4709e0a726bf7e5af
Kevin Lai <kevinlai76@gmail.com>
2019-09-06 12:28:08 -0400

    added validations for signup, created a create function for shoutouts, changed routes, other minor fixes

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,8 +2,8 @@ class AuthController < ApplicationController
     def login
         user = User.find_by(username: params[:username])
         authenticated = user.authenticate(params[:password])
-
-        if authenticated
+        
+        if authenticated 
             render json: { token: encode_token(user_payload(user)) }
         else
             render json: { error: "Wrong username or password!" }
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     has_many :shoutouts
     has_many :secrets
+
+    validates :username, presence: true, uniqueness: true
+    validates :password, presence: true
 end
