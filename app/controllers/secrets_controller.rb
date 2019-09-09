3c4f274f293a432280aaecdc7c1edb31c34729a3
Kevin Lai <kevinlai76@gmail.com>
2019-09-09 11:53:34 -0400

    minor changes destroy method in shoutouts/secrets in progress

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -4,21 +4,20 @@ class SecretsController < ApplicationController
         render json: secrets, include: [:secret_comments, :secret_likes, :secret_dislikes]
     end
 
-    def show
-        secret = Secret.find(params[:id])
-        render json: secret, include: [:secret_comments]
-    end
+    # def show
+    #     secret = Secret.find(params[:id])
+    #     render json: secret, include: [:secret_comments]
+    # end
 
     def create
-        byebug
         secret = Secret.create(secret_params)
-        render json: secret
+        render json: secret, include: [:secret_comments, :secret_likes, :secret_dislikes]
     end
 
     private
     
     def secret_params
-        params.permit(:content, :user_id)    
+        params.permit(:id, :content, :user_id)    
     end
 
 end
--- app/controllers/shoutouts_controller.rb
+++ app/controllers/shoutouts_controller.rb
@@ -9,9 +9,15 @@ class ShoutoutsController < ApplicationController
         render json: shoutout, include: [:shoutout_comments, :shoutout_likes, :shoutout_dislikes]
     end
 
+    # def destroy
+    #     shoutout = Shoutout.find(shoutout_params["id"])
+    #     shoutout.destroy
+    #     render json: shoutout
+    # end
+
     private
 
     def shoutout_params
-        params.permit(:content, :user_id)
+        params.permit(:id, :content, :user_id)
     end
 end
